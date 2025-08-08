0b594d773d967019c39f6fb7ebaa1a63f5ebb86e
Nawedy <data.whisperer.pro@gmail.com>
2025-08-07 21:33:12 -0500

    build(vercel): add @sveltejs/adapter-vercel and switch to svelte-kit build/preview; keep framework config

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,14 @@
 // File: svelte.config.js
 
 import preprocess from 'svelte-preprocess';
+import adapter from '@sveltejs/adapter-vercel';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: preprocess(),
 
 	kit: {
+		adapter: adapter(),
 		alias: {
 			$components: 'src/app/components',
 			$lib: 'src/lib',
